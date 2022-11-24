e4c80dca70b85a8a3ecae252d9512e27613b9531
Lucas <scalalucasmartin@gmail.com>
2022-11-24 12:57:26 -0300

    2commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+//Declaracion de variable navbar
+
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
 }
+
+//Declaracion de variable lupa
 
 let searchForm = document.querySelector('.search-form');
 
